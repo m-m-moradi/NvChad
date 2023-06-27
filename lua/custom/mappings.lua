adfa8c28f3739e7bec9892d5b7a768e62b1545e9
Mehdi Moradi <mahdi.moradi.w@gmail.com>
2023-06-27 11:06:07 +0330

    add mappings

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -8,5 +8,39 @@ M.general = {
 }
 
 -- more keybinds!
+M.disabled = {
+  n = {
+    ["<leader>h"] = "",
+    ["<leader>f"] = "",
+    ["<leader>uu"] = "",
+  },
+}
+
+M.competitest = {
+  -- plugin = true,
+  n = {
+    ["<leader>tr"] = { ":CompetiTestRun <CR>", "competitest test cases run" },
+    ["<leader>ts"] = { ":CompetiTestRunNE <CR>", "competitest test cases show" },
+  },
+}
+
+M.comment = {
+  -- toggle comment in both modes
+  -- plugin = true,
+  n = {
+    ["<C-_>"] = {
+      function()
+        require("Comment.api").toggle.linewise.current()
+      end,
+      "toggle comment",
+    },
+  },
+  v = {
+    ["<C-_>"] = {
+      "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
+      "toggle comment",
+    },
+  },
+}
 
 return M
