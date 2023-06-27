4a29874c779bc7418730d30d52002f499560b783
Mehdi Moradi <mahdi.moradi.w@gmail.com>
2023-06-27 11:00:26 +0330

    add snippets

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -5,3 +5,13 @@
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+--
+
+-- vscode format i.e json files
+vim.g.vscode_snippets_path = "./snips/vscode/"
+
+-- snipmate format 
+vim.g.snipmate_snippets_path = "./snips/snippets/"
+
+-- lua format 
+vim.g.lua_snippets_path = "./snips/luasnippets/"
