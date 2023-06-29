c1a7e22da270f9f327bdb1ebef2d4b66b06cec1c
Mehdi Moradi <mahdi.moradi.w@gmail.com>
2023-06-29 11:13:31 +0330

    edit snippets keybinding

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -18,6 +18,37 @@ local filterd_format = function(bufnr)
   }
 end
 
+local edit_snippets = function()
+  require("luasnip.loaders").edit_snippet_files {
+    edit = function(file)
+      vim.cmd("vsp " .. file)
+    end,
+    extend = function(ft, paths)
+      -- see: https://github.com/L3MON4D3/LuaSnip/blob/master/DOC.md#edit_snippets
+      -- label (appear in selection prompt), path that will passed to edit()
+      local snippets_file = string.format("%s/%s.snippets", vim.g.snipmate_snippets_path, ft)
+      local json_file = string.format("%s/%s.json", vim.g.vscode_snippets_path, ft)
+      local lua_file = string.format("%s/%s.lua", vim.g.lua_snippets_path, ft)
+      local new_paths = {}
+
+      local files = {
+        { "$CONFIG/" .. ft .. ".snippets", snippets_file },
+        { "$CONFIG/" .. ft .. ".json", json_file },
+        { "$CONFIG/" .. ft .. ".lua", lua_file },
+      }
+
+      for _, file in ipairs(files) do
+        local label, path = file[1], file[2]
+        if not vim.tbl_contains(paths, path) then
+          table.insert(new_paths, { label, path })
+        end
+      end
+
+      return new_paths
+    end,
+  }
+end
+
 local M = {}
 
 M.general = {
@@ -45,6 +76,8 @@ M.general = {
     ["<leader>df"] = { function() vim.diagnostic.open_float { border = "rounded" } end, "Floating diagnostic" },
     ["<leader>ic"] = { function() vim.lsp.buf.incoming_calls() end, "LSP incoming calls" },
     ["<leader>oc"] = { function() vim.lsp.buf.outgoing_calls() end, "LSP outgoing calls" },
+    ["<leader>es"] = { function() edit_snippets() end , "edit snippet" },
+    ["<leader>sp"] = { ":lua print(vim.fn.bufname())<CR>", "show path" },
     -- stylua: ignore end
   },
   v = {
